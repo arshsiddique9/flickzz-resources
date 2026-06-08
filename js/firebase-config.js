1198da523fff0188eb4f6dfa8b8fb397fb5212ba
arshsiddique9 <arshhbibi18@gmail.com>
2026-06-08 17:06:41 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -20,12 +20,12 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-
 
 // 🔧 Your Firebase config object (REPLACE THESE VALUES)
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCMpZtzf3CSrVztGihysYL3M4gC6ZgUzG0",
+    authDomain: "flickzz-resources.firebaseapp.com",
+    projectId: "flickzz-resources",
+    storageBucket: "flickzz-resources.firebasestorage.app",
+    messagingSenderId: "554417592538",
+    appId: "1:554417592538:web:be87ac778712df5c032de3"
 };
 
 // ============================================
